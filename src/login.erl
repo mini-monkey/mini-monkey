734105e1e8a5a635aac5fdd67aea355d3ec7208d
Niklas Johansson <raphexion@gmail.com>
2019-06-26 13:24:02 +0200

    Add login server

---
+++ src/login.erl
@@ -0,0 +1,54 @@
+-module(login).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+-export([start_link/0,
+	 attempt/1,
+	 add_token/1]).
+
+-export([init/1,
+	 handle_call/3,
+	 handle_cast/2,
+	 handle_info/2,
+	 terminate/2,
+	 code_change/3]).
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+attempt(Token) ->
+    gen_server:call(?MODULE, {attempt, Token}).
+
+add_token(Token) ->
+    gen_server:cast(?MODULE, {add, Token}).
+
+init(_) ->
+    ets:new(?MODULE, [set, named_table]),
+    {ok, #{}}.
+
+handle_call({attempt, Token}, _From, State) ->
+    case ets:lookup(?MODULE, Token) of
+	[{Key, true}] ->
+	    {reply, ok, State};
+	_ ->
+	    {reply, error, State}
+    end;
+
+handle_call(What, _From, State) ->
+    {reply, {eror, What}, State}.
+
+handle_cast({add, Token}, State) ->
+    ets:insert(?MODULE, {Token, true}),
+    {noreply, State};
+
+handle_cast(_What, State) ->
+    {noreply, State}.
+
+handle_info(_What, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
