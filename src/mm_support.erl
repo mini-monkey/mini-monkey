b3141ea3c6f4194864a4f8bbd1e15150c3888d5a
Niklas Johansson <raphexion@gmail.com>
2019-06-27 20:22:16 +0200

    Move correctly_formated

--- src/mm_support.erl
+++ src/mm_support.erl
@@ -1,6 +1,9 @@
 -module(mm_support).
 
--export([binary_env_var/2]).
+-include_lib("eunit/include/eunit.hrl").
+
+-export([binary_env_var/2,
+	 correctly_formated/1]).
 
 binary_env_var(Name, Default) when is_list(Default) ->
     binary_env_var(Name, list_to_binary(Default));
@@ -12,3 +15,21 @@ binary_env_var(Name, Default) ->
 	Var ->
 	    list_to_binary(Var)
     end.
+
+correctly_formated(<<Code:8, Size:16/little, Data/binary>>) when Size < byte_size(Data) ->
+    <<Payload:Size/binary, Rest/binary>> = Data,
+    {ok, Code, Payload, Rest};
+
+correctly_formated(<<Code:8, Size:16/little, Payload/binary>>) when Size == byte_size(Payload) ->
+    {ok, Code, Payload, <<>>};
+
+correctly_formated(Data) ->
+    {error, Data}.
+
+%%------------------------------------------------------------------------------
+%% Tests
+%%------------------------------------------------------------------------------
+
+formated_empty_test() ->
+    Data = <<1,2,3,4>>,
+    ?assert({error, Data} =:= correctly_formated(Data)).
