da6b951c9d5e38ff4745ad0100f51aa16ff8bb20
Niklas Johansson <raphexion@gmail.com>
2019-06-27 11:08:52 +0200

    Update tests

--- test/minimal_SUITE.erl
+++ test/minimal_SUITE.erl
@@ -9,18 +9,18 @@ all() -> [test_failed_auth,
 	  test_successful_auth].
 
 test_failed_auth(_Config) ->
-    application:ensure_started(minimonkey),
+    application:ensure_all_started(minimonkey),
     {ok, Sock} = gen_tcp:connect("localhost", 1773, [binary]),
 
     Token = <<"guest">>,
-    ok = gen_tcp:send(Sock, minimonkey_encode:login(Token)),
-    {ok, minimonkey_encode:login_failure()} =:= gen_tcp:recv(Sock, 0).
+    ok = gen_tcp:send(Sock, mm_encode:login(Token)),
+    {ok, mm_encode:login_failure()} =:= gen_tcp:recv(Sock, 0).
 
 test_successful_auth(_Config) ->
-    application:ensure_started(minimonkey),
+    application:ensure_all_started(minimonkey),
     {ok, Sock} = gen_tcp:connect("localhost", 1773, [binary]),
 
     Token = <<"guest">>,
     ok = login:add_token(Token),
-    ok = gen_tcp:send(Sock, minimonkey_encode:login(Token)),
-    {ok, minimonkey_encode:login_successful()} =:= gen_tcp:recv(Sock, 0).
+    ok = gen_tcp:send(Sock, mm_encode:login(Token)),
+    {ok, mm_encode:login_successful()} =:= gen_tcp:recv(Sock, 0).
