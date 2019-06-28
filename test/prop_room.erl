de50a8ffbb03235ff6e14aa7b75fa0518534d13b
Niklas Johansson <raphexion@gmail.com>
2019-06-28 08:30:45 +0200

    Add test for many 1 pub, 1 sub, many messages

--- test/prop_room.erl
+++ test/prop_room.erl
@@ -4,7 +4,7 @@
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
-prop_file_test() ->
+prop_room_minimal_test() ->
     ?FORALL({Name, Content, Tag}, {blob(), blob(), blob()},
 	    begin
 		{ok, User} = mock_user:start_link(),
@@ -16,5 +16,32 @@ prop_file_test() ->
 		[{published, Content, Tag}] =:= Result
 	    end).
 
+prop_room_minimal_multi_test() ->
+    ?FORALL({Name, Contents, Tag}, {blob(), blobs(), blob()},
+	    begin
+		{ok, User} = mock_user:start_link(),
+		{ok, Room} = room:start_link(Name),
+		ok = room:subscribe(Room, User, Tag),
+		ok = publish_many(Room, Contents),
+		timer:sleep(10),
+		Result = mock_user:messages(User),
+		Result =:= contents_to_result(Contents, Tag)
+	    end).
+
+publish_many(_Room, []) ->
+    ok;
+publish_many(Room, [Content|Rest]) ->
+    ok = room:publish(Room, Content),
+    publish_many(Room, Rest).
+
+contents_to_result(Contents, Tag) ->
+    lists:map(fun(Content) ->
+		      {published, Content, Tag}
+	      end,
+	      Contents).
+
 blob() ->
     non_empty(binary()).
+
+blobs() ->
+    non_empty(list(blob())).
