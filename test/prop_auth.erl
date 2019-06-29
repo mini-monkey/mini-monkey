23e6462ef8922508657a6475eb93859356b267c7
Niklas Johansson <raphexion@gmail.com>
2019-06-29 21:43:11 +0200

    Refactor test-use for god_token

--- test/prop_auth.erl
+++ test/prop_auth.erl
@@ -1,6 +1,6 @@
 -module(prop_auth).
 -include_lib("proper/include/proper.hrl").
--define(GOD_TOKEN, <<"rNtiPfxtfRBXrb4e9wh8qnTSir83vTeU4WHj7pwq">>).
+-import(mm_test_tokens, [god_token/0]).
 
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
@@ -8,7 +8,7 @@
 prop_admin_with_wrong_token_test() ->
     ?FORALL({Token, Name}, {blob(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, Room} = mm_room_sup:create_room(Name),
 		true andalso
 		    error =:= mm_room:permissions(Room, Token, add, to_admin, Token) andalso
@@ -22,9 +22,9 @@ prop_admin_with_wrong_token_test() ->
 prop_admin_with_correct_token_test() ->
     ?FORALL({Token, Name}, {blob(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, Room} = mm_room_sup:create_room(Name),
-		ok = mm_room:permissions(Room, ?GOD_TOKEN, add, to_admin, Token),
+		ok = mm_room:permissions(Room, god_token(), add, to_admin, Token),
 		true andalso
 		    ok =:= mm_room:permissions(Room, Token, add, to_admin, Token) andalso
 		    ok =:= mm_room:permissions(Room, Token, add, to_pub, Token) andalso
@@ -38,7 +38,7 @@ prop_admin_with_correct_token_test() ->
 prop_subscribe_with_wrong_token_test() ->
     ?FORALL({Token, Name, Tag}, {blob(), blob(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, User} = mock_user:start_link(),
 		{ok, Room} = mm_room_sup:create_room(Name),
 		error =:= mm_room:subscribe(Room, Token, User, Tag)
@@ -47,7 +47,7 @@ prop_subscribe_with_wrong_token_test() ->
 prop_publish_with_wrong_token_test() ->
     ?FORALL({Token, Name, Content}, {blob(), blob(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, Room} = mm_room_sup:create_room(Name),
 		error =:= mm_room:publish(Room, Token, Content)
 	    end).
@@ -55,12 +55,12 @@ prop_publish_with_wrong_token_test() ->
 prop_subscribe_with_new_token_test() ->
     ?FORALL({Token, Name, Tag}, {blob(), blob(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, User} = mock_user:start_link(),
 		{ok, Room} = mm_room_sup:create_room(Name),
-		ok = mm_room:permissions(Room, ?GOD_TOKEN, add, to_sub, Token),
+		ok = mm_room:permissions(Room, god_token(), add, to_sub, Token),
 		ok =:= mm_room:subscribe(Room, Token, User, Tag) andalso
-		    ok =:= mm_room:permissions(Room, ?GOD_TOKEN, revoke, to_sub, Token) andalso
+		    ok =:= mm_room:permissions(Room, god_token(), revoke, to_sub, Token) andalso
 		    error =:= mm_room:subscribe(Room, Token, User, Tag) andalso
 		    ok =:= mm_room:unsubscribe(Room, User)
 	    end).
@@ -68,11 +68,11 @@ prop_subscribe_with_new_token_test() ->
 prop_publish_with_new_token_test() ->
     ?FORALL({Token, Name, Content}, {blob(), blob(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, Room} = mm_room_sup:create_room(Name),
-		ok = mm_room:permissions(Room, ?GOD_TOKEN, add, to_pub, Token),
+		ok = mm_room:permissions(Room, god_token(), add, to_pub, Token),
 		ok =:= mm_room:publish(Room, Token, Content) andalso
-		    ok =:= mm_room:permissions(Room, ?GOD_TOKEN, revoke, to_pub, Token) andalso
+		    ok =:= mm_room:permissions(Room, god_token(), revoke, to_pub, Token) andalso
 		    error =:= mm_room:publish(Room, Token, Content)
 	    end).
 
--- test/prop_room.erl
+++ test/prop_room.erl
@@ -1,6 +1,6 @@
 -module(prop_room).
 -include_lib("proper/include/proper.hrl").
--define(GOD_TOKEN, <<"rNtiPfxtfRBXrb4e9wh8qnTSir83vTeU4WHj7pwq">>).
+-import(mm_test_tokens, [god_token/0]).
 
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
@@ -9,12 +9,12 @@
 prop_room_minimal_test() ->
     ?FORALL({Name, Content, Tag}, {blob(), blob(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, User} = mock_user:start_link(),
 		{ok, Room} = mm_room_sup:create_room(Name),
 		{ok, 0} = mm_room:count_subscribers(Name),
-		ok =  mm_room:subscribe(Room, ?GOD_TOKEN, User, Tag),
-		ok = mm_room:publish(Room, ?GOD_TOKEN, Content),
+		ok =  mm_room:subscribe(Room, god_token(), User, Tag),
+		ok = mm_room:publish(Room, god_token(), Content),
 		timer:sleep(10),
 		Result = mock_user:messages(User),
 		mm_room:unsubscribe(Room, User),
@@ -25,12 +25,12 @@ prop_room_minimal_test() ->
 prop_room_minimal_multi_test() ->
     ?FORALL({Name, Contents, Tag}, {blob(), blobs(), blob()},
 	    begin
-		mm_room_sup:start_link(?GOD_TOKEN),
+		mm_room_sup:start_link(god_token()),
 		{ok, User} = mock_user:start_link(),
 		{ok, Room} = mm_room_sup:create_room(Name),
 		{ok, 0} = mm_room:count_subscribers(Name),
-		ok = mm_room:subscribe(Room, ?GOD_TOKEN, User, Tag),
-		ok = publish_many(Room, ?GOD_TOKEN, Contents),
+		ok = mm_room:subscribe(Room, god_token(), User, Tag),
+		ok = publish_many(Room, god_token(), Contents),
 		timer:sleep(10),
 		Result = mock_user:messages(User),
 		mm_room:unsubscribe(Room, User),
